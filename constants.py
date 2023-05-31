6fb2d67c567b814616fa7660584f8ebdd98da4c1
Rohith Ramakrishnan <55501708+Rohith-2@users.noreply.github.com>
2023-05-31 07:33:13 +0000

    Init-Commit

---
+++ constants.py
@@ -0,0 +1,12 @@
+import os
+from chromadb.config import Settings
+from config import PERSIST_DIRECTORY
+
+
+
+# Define the Chroma settings
+CHROMA_SETTINGS = Settings(
+        chroma_db_impl='duckdb+parquet',
+        persist_directory=PERSIST_DIRECTORY,
+        anonymized_telemetry=False
+)
