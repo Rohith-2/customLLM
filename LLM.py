38b2b86233f8cbcdf8242fff65e0c7005f2a4238
Rohith Ramakrishnan <rrohith2001@gmail.com>
2023-06-05 14:31:51 +0530

    Custom-Changes

--- LLM.py
+++ LLM.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3
+from constants import CHROMA_SETTINGS
 from langchain.chains import RetrievalQA
 from langchain.embeddings import HuggingFaceEmbeddings
 from langchain.callbacks.streaming_stdout import StreamingStdOutCallbackHandler
@@ -6,7 +7,17 @@ from langchain.vectorstores import Chroma
 from langchain.llms import GPT4All, LlamaCpp
 import os
 import argparse
-from config import PERSIST_DIRECTORY,MODEL_TYPE,MODEL_PATH,EMBEDDINGS_MODEL_NAME,MODEL_N_CTX
+from config import (
+    PERSIST_DIRECTORY,
+    MODEL_TYPE,
+    MODEL_PATH,
+    EMBEDDINGS_MODEL_NAME,
+    MODEL_N_CTX,
+    chunk_size,
+    chunk_overlap,
+    target_source_chunks,
+    n_gpu_layers
+)
 
 
 embeddings_model_name = EMBEDDINGS_MODEL_NAME
@@ -17,26 +28,46 @@ model_type = MODEL_TYPE
 model_path = MODEL_PATH
 model_n_ctx = MODEL_N_CTX
 
-from constants import CHROMA_SETTINGS
 
 def main():
     # Parse the command line arguments
     args = parse_arguments()
     embeddings = HuggingFaceEmbeddings(model_name=embeddings_model_name)
-    db = Chroma(persist_directory=persist_directory, embedding_function=embeddings, client_settings=CHROMA_SETTINGS)
-    retriever = db.as_retriever()
+    db = Chroma(
+        persist_directory=persist_directory,
+        embedding_function=embeddings,
+        client_settings=CHROMA_SETTINGS,
+    )
+    retriever = db.as_retriever(search_kwargs={"k": target_source_chunks})
     # activate/deactivate the streaming StdOut callback for LLMs
     callbacks = [] if args.mute_stream else [StreamingStdOutCallbackHandler()]
     # Prepare the LLM
-    #match model_type:
-    if model_type== "LlamaCpp":
-        llm = LlamaCpp(model_path=model_path, n_ctx=model_n_ctx, callbacks=callbacks, verbose=False)
+    # match model_type:
+    if model_type == "LlamaCpp":
+        llm = LlamaCpp(
+            model_path=model_path,
+            n_ctx=model_n_ctx,
+            n_gpu_layers=n_gpu_layers,
+            callbacks=callbacks,
+            verbose=False,
+        )
     elif model_type == "GPT4All":
-        llm = GPT4All(model=model_path, n_ctx=model_n_ctx, backend='gptj', callbacks=callbacks, verbose=False)
+        llm = GPT4All(
+            model=model_path,
+            n_ctx=model_n_ctx,
+            backend="gptj",
+            callbacks=callbacks,
+            verbose=False,
+        )
     else:
         print(f"Model {model_type} not supported!")
-        exit;
-    qa = RetrievalQA.from_chain_type(llm=llm, chain_type="stuff", retriever=retriever, return_source_documents= not args.hide_source)
+        exit
+    qa = RetrievalQA.from_chain_type(
+        llm=llm,
+        chain_type="stuff",
+        retriever=retriever,
+        return_source_documents=not args.hide_source,
+    )
     # Interactive questions and answers
     while True:
         query = input("\nEnter a query: ")
@@ -45,8 +76,11 @@ def main():
 
         # Get the answer from the chain
         res = qa(query)
-        
-        answer, docs = res['result'], [] if args.hide_source else res['source_documents']
+
+        answer, docs = (
+            res["result"],
+            [] if args.hide_source else res["source_documents"],
+        )
 
         # Print the result
         print("\n\n> Question:")
@@ -57,17 +91,27 @@ def main():
         # Print the relevant sources used for the answer
         for document in docs:
             print("\n> " + document.metadata["source"] + ":")
-           # print(document.page_content)
+            print(document.page_content)
+
 
 def parse_arguments():
-    parser = argparse.ArgumentParser(description='privateGPT: Ask questions to your documents without an internet connection, '
-                                                 'using the power of LLMs.')
-    parser.add_argument("--hide-source", "-S", action='store_true',
-                        help='Use this flag to disable printing of source documents used for answers.')
+    parser = argparse.ArgumentParser(
+        description="privateGPT: Ask questions to your documents without an internet connection, "
+        "using the power of LLMs."
+    )
+    parser.add_argument(
+        "--hide-source",
+        "-S",
+        action="store_true",
+        help="Use this flag to disable printing of source documents used for answers.",
+    )
 
-    parser.add_argument("--mute-stream", "-M",
-                        action='store_true',
-                        help='Use this flag to disable the streaming StdOut callback for LLMs.')
+    parser.add_argument(
+        "--mute-stream",
+        "-M",
+        action="store_true",
+        help="Use this flag to disable the streaming StdOut callback for LLMs.",
+    )
 
     return parser.parse_args()
 
